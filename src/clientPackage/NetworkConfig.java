a206629bc1cad4159409c304c6ebfffcc5855fcb
Elias Geiger <elias@ele-dev.de>
2020-08-26 12:16:17 +0200

    Added hashing salt config value

--- src/clientPackage/NetworkConfig.java
+++ src/clientPackage/NetworkConfig.java
@@ -5,6 +5,7 @@ public interface NetworkConfig {
 	// Global network configuration constants
 	public static final String serverAddress = "ele-server.de";
 	public static final int serverPort = 1044;
+	public static final String hashingSalt = "g_p8nrY3";
 	// ...
 	
 }
