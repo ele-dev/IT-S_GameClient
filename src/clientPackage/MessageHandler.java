b1eb46beffce11717a5add18114fc09a7d7e027b
Elias Geiger <elias@ele-dev.de>
2020-08-29 20:07:17 +0200

    Implemented Account Stats message and updated message handler accordingly

--- src/clientPackage/MessageHandler.java
+++ src/clientPackage/MessageHandler.java
@@ -14,12 +14,18 @@ public class MessageHandler {
 			case GenericMessage.MSG_ACCOUNT_STATS:
 			{
 				// First parse the message into the right format
+				MsgAccountStats accountStats = (MsgAccountStats) msg;
+				
+				// Now store the received stats from the message
 				// ...
+				
+				// Show stats on the console for debugging
+				System.out.println("Received account stats from the server");
+				System.out.println("Played Matches: " + accountStats.getPlayedMatches());
+				System.out.println("Account Balance: " + accountStats.getAccountBalance());
 				
 				break;
 			}
-			
-			// ...
 			
 			default:
 			{
