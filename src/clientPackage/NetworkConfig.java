17c553fd4f9163dacfd7689ec181b967e68e9d3a
Elias Geiger <elias@ele-dev.de>
2020-09-02 12:46:33 +0200

    Changed Sevice port because of interference with other application

--- src/clientPackage/NetworkConfig.java
+++ src/clientPackage/NetworkConfig.java
@@ -13,7 +13,7 @@ public interface NetworkConfig {
 	
 	// Global network configuration constants
 	public static final String serverAddress = "ele-server.de";
-	public static final int serverPort = 1044;
+	public static final int serverPort = 50044;
 	public static final String hashingSalt = "g_p8nrY3";
 	// ...
 	
