672c0d2df6f543bda3318fcd6367585511710f08
Elias Geiger <elias@ele-dev.de>
2020-09-22 16:43:16 +0200

    Added Window close event for auto disconnect

--- src/main/MainJFrame.java
+++ src/main/MainJFrame.java
@@ -1,8 +1,13 @@
 package main;
 
 import java.awt.Container;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import clientPackage.Connection;
 
 @SuppressWarnings("serial")
 public class MainJFrame extends JFrame {
@@ -10,11 +15,16 @@ public class MainJFrame extends JFrame {
 	private int w = 600;
 	private int h = 600;
 	
+	// Network connection
+	private static Connection conn = null;
+	
+	// Application Gui panels
 	StagePanel stagePanel;
 	
+	// Constructor
 	public MainJFrame() {
 		setSize(w,h);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		setLayout(null);
 		setLocationRelativeTo(null);
 		setTitle("TikTakToe Client");
@@ -27,9 +37,43 @@ public class MainJFrame extends JFrame {
 		cp.add(stagePanel);
 	}
 	
-	
-	@SuppressWarnings("unused")
+	// Main method application entry point //
 	public static void main(String[] args) {
+		
+		// Create connection instance
+		conn = new Connection();
+		
+		// Print status of network connection
+		if(conn.isConnected() == true) {
+			JOptionPane.showMessageDialog(null, "Connect to server");
+		} else {
+			// JOptionPane.showMessageDialog(null, "Failed to connect to server!");
+			System.exit(0);
+		}
+		
+		// let the player choose his team (x or o)
+		conn.chooseTeam();
+		
+		// Create the application window
 		MainJFrame f = new MainJFrame();
+		System.out.println("Window created");
+		
+		// Add a window listener to the frame
+		f.addWindowListener(new WindowAdapter() {
+			
+			// Define window close event
+			@Override
+			public void windowClosing(WindowEvent windowEvent) {
+				f.setVisible(false);
+				System.out.println("window was closed --> cleanup routine");
+				
+				// close the connection
+				conn.finalize();
+				
+				// Finally exit the application 
+				System.out.println("Application close up");
+				System.exit(0);
+			}
+		});
 	}
 }
