e78d949e6185aff9f29c83f6056aed77af40be97
Elias Geiger <elias@ele-dev.de>
2020-10-12 16:32:40 +0200

    Implemented Method for relative positioning of gui elements

--- src/menueGui/GuiElement.java
+++ src/menueGui/GuiElement.java
@@ -14,6 +14,8 @@ package menueGui;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
+import Stage.ProjectFrame;
+
 public abstract class GuiElement {
 	
 	// class members
@@ -34,6 +36,35 @@ public abstract class GuiElement {
 	
 	// Drawing method
 	public abstract void draw(Graphics2D g2d);
+	
+	// Function for positioning a gui element relatively to the window frame
+	public void setRelativePosition(int x, int y) {
+		
+		// calculate the abosolute coordinates in the frame
+		float xFactor = 0.01f * x;
+		float yFactor = 0.01f * y;
+		int posX = Math.round(xFactor * ProjectFrame.width);
+		int posY = Math.round(yFactor * ProjectFrame.height);
+		
+		// Center the gui element using the own dimensions
+		posX -= this.rect.width / 2;
+		posY -= this.rect.height / 2;
+		
+		// Dont exit the frame 
+		if(posX < 0)
+			posX = 0;
+		if(posY < 0)
+			posY = 0;
+		if(posX > ProjectFrame.width)
+			posX = ProjectFrame.width;
+		if(posY > ProjectFrame.height) 
+			posY = ProjectFrame.height;
+		
+		// Apply the coordinates to this GUI element
+		this.rect.x = posX;
+		this.rect.y = posY;
+		
+	}
 	
 	// Getter & setter
 	public void setEnabled(boolean status) {
